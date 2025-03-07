8298b945b54be8902d6f3ae725e9cba79317bcbe
«simenaas95» <simenaas95@gmail.com>
2025-03-07 10:08:51 +0100

    endret en linje i spinAPI.mjs

--- SpinAPI.mjs
+++ SpinAPI.mjs
@@ -2,13 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import wheelRouter from './routes/wheelRoutes.mjs';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public', { extensions: ['html'] }));
 
 
 app.use('/api/wheels', wheelRouter);
@@ -18,7 +23,9 @@ const wheelOptions = [
     'Win 100 points', 'Win 200 points', 'Lose 50 points', 'Try Again',
     'Jackpot!', 'Win a Bonus Spin', 'Win 500 points', 'Lose a Turn'
 ];
-
+app.get('/spin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'spin.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Spin the Wheel API! Use /spin to spin the wheel.');
